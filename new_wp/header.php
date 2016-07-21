7869cff72a00cc9ad2d7787ab255b957fc56433d
Ryu Ishikawa <ryu.ishikawa@howtelevision.jp>
2016-07-21 10:37:11 +0900

    fix some modify

--- new_wp/header.php
+++ new_wp/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <head lang="ja">
   <meta charset="<?php bloginfo('charset');?>">
-  <meta name="viewport" content="width=device-width">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
   <title><?
       if(is_single()){
         wp_title('|',true,'right');
